3436976afe27e430acda8edc20420facad49e1fd
warits <antiwinter@126.com>
2018-11-06 14:18:11 +0800

    test from net

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
-const stdf = require('../')
+const stdf = require('stdfjs')
 
-let rs = fs.createReadStream('./1.stdf')
+let rs = fs.createReadStream('../1.stdf')
 
 stdf.parser(rs).on('data', rec => {
   console.log('%j', rec)
